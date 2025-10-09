54ba1d00a6793f85f13fe73d71af1c0f1a0899a2
JonathanC3po <Jonathan.conte1938@gmail.com>
2025-10-09 15:12:47 +0000

    jtc

--- WordSorter.java
+++ WordSorter.java
@@ -1,10 +1,13 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class WordSorter 
 {
 	public static void main(String[] args) 
 	{
 		Scanner in = new Scanner(System.in); 
+		Scanner keyB = new Scanner(System.in); 
 		try
 		{
 			in = new Scanner(new File("article.txt"));	
@@ -14,10 +17,12 @@ public class WordSorter
 			System.out.println("Cannot find file... Exiting Program");
 			return;
 		}	
+		ArrayList<String> words = new ArrayList<String>();
 		String word = "";
 		while(in.hasNext())
 		{
 			word = in.next();
+
 			word = word.replace(",", "");
 			word = word.replace("(tm)", "");
 			word = word.replace(")", "");
@@ -30,7 +35,31 @@ public class WordSorter
 			word = word.replace("'", "");
 			System.out.println(word);
 			
+			words.add(word);
 			
 		}
+		List<String> lWords = new ArrayList<>();
+        for (String wordy : words) {
+            lWords.add(wordy.toLowerCase());
+        }
+		for(int i = 0;i<lWords.size();i++){
+			for(int i = 0;i<lWords.size()-1;i++){
+				String x = (lWords.get(i)).chartAt(0)
+				String y = (lWords.get(i+1)).chartAt(0)
+				
+				if(x < y){
+					lWords.set(
+					)
+				}
+			}
+		}
+		System.out.println("type 1 if you want to show the list");
+		int val = keyB.nextInt();
+		if (val == 1){
+			System.out.println(lWords);
+		}
+		
+		
+
 	}
 }
